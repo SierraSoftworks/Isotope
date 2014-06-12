28d3e5b1d258f14b11a68c1f4308afd5a8ad58d3
Benjamin Pannell <admin@sierrasoftworks.com>
2014-06-12 18:06:06 +0200

    Added C library

---
+++ src/libs/c/isotope.c
@@ -0,0 +1,134 @@
+#ifndef ISOTOPE_C
+#define ISOTOPE_C
+
+#include "isotope.c"
+#include <stdio.h>
+
+#ifdef RPI
+#define ISOTOPE_IO
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+
+#else
+#define ISOTOPE_FIO
+
+#endif
+
+/*
+ * Internal Functions
+ */
+int _isotope_pack(short axis1, short axis2, short axis3) {
+	return 0x00000000 | ((axis1 & 0x3ff) << 20) | ((axis2 & 0x3ff) << 10) | (axis3 & 0x3ff);
+}
+
+/*
+ * Public API Functions
+ */
+
+int isotope_open(const char* device) {
+	int uart;
+
+	#ifdef ISOTOPE_FIO // f Prefixed IO operations (fopen, fclose etc.)
+	uart = (int)fopen(device, 'w');
+	if(!uart) return NULL;
+
+	#elif ISOTOPE_IO // Standard IO operations
+	uart = open(device, O_WRONLY | O_NOCTTY | O_NDELAY)
+	if(-1 == uart) return NULL;
+	#endif
+
+	/**
+	 * Raspberry Pi specific UART configuration
+	 * Adapted from http://www.raspberry-projects.com/pi/programming-in-c/uart-serial-port/using-the-uart
+	 */ 
+
+	#ifdef RPI
+	struct termios options;
+
+	tcgetattr(uart, &options);
+	options.c_cflag = B57600 | CS8 | CLOCAL;		//<Set baud rate
+	options.c_iflag = IGNPAR;
+	options.c_oflag = 0;
+	options.c_lflag = 0;
+	tcflush(uart0_filestream, TCIFLUSH);
+	tcsetattr(uart0_filestream, TCSANOW, &options);
+	#endif
+
+	return uart;
+}
+
+char isotope_close(int handle) {
+	#ifdef ISOTOPE_FIO
+	return (char)fclose((FILE*)handle);
+	#elif ISOTOPE_IO
+	return close(handle);
+	#endif
+}
+
+char isotope_write(int isotope, const char* packet, char packet_length) {
+	#ifdef ISOTOPE_FIO
+	return fwrite(packet, sizeof(char), packet_length, (FILE*)isotope);
+	#elif ISOTOPE_IO
+	return write(isotope, packet, packet_length);
+	#endif
+}
+
+char isotope_mouse(int isotope, char buttons, char deltaX, char deltaY, char deltaScroll) {
+	char packet[5] = { 0x40 }, length = 4;
+
+	packet[1] = buttons;
+	packet[2] = deltaX;
+	packet[3] = deltaY;
+	packet[4] = deltaScroll;
+
+	// Compress the packet to only send the required data
+	if(!deltaScroll) {
+		length--;
+		if(!deltaY) {
+			length--;
+			if(!deltaX) {
+				length--;
+				if(!buttons) length--;
+			}
+		}
+	}
+
+	// Populate the header's length field
+	packet[0] |= length;
+
+	return isotope_write(isotope, packet, length + 1);
+}
+
+char isotope_keyboard(int isotope, char modifiers, const char[] keys, char keys_count) {
+	char packet[8] = { 0x20 }, i = 0;
+	packet[1] = modifiers;
+
+	if(!keys_count && !modifiers) return isotope_write(isotope, packet, 1);
+
+	for(i = 0; i < keys_count; i++) packet[i + 2] = keys[i];
+	packet[0] |= keys_count + 1;
+
+	return isotope_write(isotope, packet, keys_count + 2);
+}
+
+char isotope_joystick(int isotope, int buttons, short x, short y, short z, short rz, short sliderLeft, short sliderRight, char hat) {
+	char packet[14] = { 0x60 | 13 };
+	
+	// Copy buttons into packet
+	*((int*)(packet + 1)) = buttons;
+
+	// Copy X, Y, Z axis pack into packet
+	*((int*)(packet + 5)) = _isotope_pack(x, y, z);
+
+	// Copy rZ, sL, sR axis pack into packet
+	*((int*)(packet + 9)) = _isotope_pack(rz, sliderLeft, sliderRight);
+
+	// Copy hat switch position into packet
+	packet[13] = hat;
+
+	return isotope_write(isotope, packet, 14);
+}
+
+#endif
---
+++ src/libs/c/isotope.h
@@ -0,0 +1,67 @@
+/**
+ * Isotope C Library
+ * Provides a C interface between the Isotope emulation chip
+ * and the local system, as well as a number of useful command
+ * wrappers.
+ *
+ * Copyright Benjamin Pannell 2014
+ */
+
+#ifndef ISOTOPE_H
+#define ISOTOPE_H
+
+/**
+ * Opens a new Isotope using the specified device to communicate, e.g. /dev/ttyUART0
+ * @param device The device to open for interaction between the Isotope and this library
+ * @returns isotope The unique Isotope ID used for submission of emulation requests
+ */
+int isotope_open(const char* device);
+/**
+ * Closes an Isotope connection given the Isotope's ID, this should be done before closing your application.
+ * @param isotope The ID of the connection to terminate
+ * @returns status Returns 0 if the connection was terminated successfully
+ */
+char isotope_close(int isotope);
+
+/**
+ * Sends a mouse emulation request using the given Isotope.
+ * @param isotope The opened Isotope connection to send the emulation request over.
+ * @param buttons The depressed mouse buttons to emulate, a set of MOUSE_ flags.
+ * @param deltaX The x-axis movement of the mouse -127 to 127.
+ * @param deltaY The y-axis movement of the mouse -127 to 127.
+ * @param deltaScroll The scroll delta, usually +/- 3 to emulate a standard scroll wheel.
+ */
+char isotope_mouse(int isotope, char buttons, char deltaX, char deltaY, char deltaScroll);
+
+/**
+ * Sends a keyboard emulation request using the given Isotope.
+ * @param isotope The opened Isotope connection to send the emulation request over.
+ * @param modifiers A combination of modifier keys to be transmitted
+ * @param keys[] The physical keyboard keys who's depression should be emulated.
+ * @param keys_count The number of physical keyboard keys to be emulated (max of 6).
+ */
+char isotope_keyboard(int isotope, char modifiers, const char[] keys, char keys_count);
+
+/**
+ * Sends a joystick emulation request using the given Isotope.
+ * @param isotope The opened Isotope connection to send the emulation request over.
+ * @param buttons The 32 button's depression states to emulate 0x1, 0x2 etc.
+ * @param x The x-axis reporting value (from 0 to 1023), 512 represents center.
+ * @param y The y-axis reporting value (from 0 to 1023), 512 represents center.
+ * @param z The z-axis reporting value (from 0 to 1023), 512 represents center.
+ * @param rz The z-axis rotation reporting value (from 0 to 1023), 512 represents center.
+ * @param sliderLeft The left slider reporting value (from 0 to 1023), 512 represents center.
+ * @param sliderRight The right slider reporting value (from 0 to 1023), 512 represents center.
+ * @param hat The hat switch position. 0xff represents center, 0 to 7 represent angles in multiples of 45 degrees.
+ */
+char isotope_joystick(int isotope, int buttons, short x, short y, short z, short rz, short sliderLeft, short sliderRight, char hat);
+
+/**
+ * Writes a packet to the given Isotope, used for low level commands
+ * @param isotope The Isotope connection over which to send the packet
+ * @param packet The packet to transmit to the given Isotope
+ * @param length The number of bytes in the packet
+ */
+char isotope_write(int isotope, const char* packet, char length);
+
+#endif
