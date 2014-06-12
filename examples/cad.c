5f8fc536a63ac37674abce910fcb2bc36619f30c
Benjamin Pannell <admin@sierrasoftworks.com>
2014-06-12 18:06:25 +0200

    Added example Ctrl+Alt+Del application

---
+++ examples/cad.c
@@ -0,0 +1,28 @@
+/**
+ * Ctrl+Alt+Del Emulation using Isotope
+ * This is a quick demonstration program showing how Isotope can be used
+ * to quickly and easily emulate the pressing of the Ctrl+Alt+Delete
+ * keyboard combo on Windows machines.
+ *
+ * IMPORTANT
+ * Ensure that you are using the RPi build of the isotope library or
+ * you may encounter issues with the UART not being configured correctly.
+ */
+
+#include "../src/libs/c/keys.h"
+#include "../src/libs/c/isotope.h"
+
+int main() {
+	int isotope;
+	char[] keys = { KEY_DELETE };
+
+	isotope = isotope_open("/dev/ttyAMA0");
+	if(!isotope) return 1;
+
+	// Send the Ctrl+Alt+Del combo
+	isotope_keyboard(isotope, MODIFIERKEY_CTRL | MODIFIERKEY_ALT, keys, 1);
+	// Release the keys
+	isotope_keyboard(isotope, 0, 0, 0);
+
+	return 0;
+}
