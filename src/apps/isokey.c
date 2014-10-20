b03c5e787251db7aafdb87cac84289f9744cd2ce
Benjamin Pannell <contact@sierrasoftworks.com>
2014-10-20 16:28:21 +0200

    Added terminal apps for Isotope

---
+++ src/apps/isokey.c
@@ -0,0 +1,203 @@
+/**
+ * Isotope Keyboard Emulation Command Line Applet
+ * This command line executable allows emulation of
+ * basic keyboard commands through a shell terminal
+ * or the system() function and its equivalents in
+ * other frameworks.
+ */
+
+/**
+ * Examples
+ * Type "Hello World!"
+ * > isokey -S H
+ * > isokey e
+ * > isokey l
+ * > isokey l
+ * > isokey o
+ * > isokey SPACE
+ * > isokey -S w
+ * > isokey o
+ * > isokey r
+ * > isokey l
+ * > isokey d
+ * > isokey -S 1
+ */
+
+#include "../libs/c/isotope.h"
+#include "../libs/c/keylayouts.h"
+
+#include "cmdline.h"
+
+#include <stdio.h>
+#include <string.h>
+
+char parseKey(char* keys, int* keysCount, int* parseLocation);
+
+int main(int argc, const char** argv) {
+    int isotope;
+    char modifiers = 0;
+    char keys[6] = {0};
+    int keysCount = 0;
+    int parseLocation = 1;
+    char release = 1;
+    
+    cmd_init(argc, argv);
+    
+    if(cmd_hasFlag('?', "help")) {
+        printf("Isotope Keyboard Emulator\n");
+        printf("Usage: %s -C -shift -A -win A SPACE BACKSPACE DEL F11 NUM7\n", cmd_application());
+        return -1;
+    }
+    
+    if(cmd_hasFlag('H', "hold")) release = 0;
+    
+    if(cmd_hasFlag('C', "ctrl")) modifiers |= MODIFIERKEY_CTRL;
+    if(cmd_hasFlag(0, "lctrl")) modifiers |= MODIFIERKEY_LEFT_CTRL;
+    if(cmd_hasFlag(0, "rctrl")) modifiers |= MODIFIERKEY_RIGHT_CTRL;
+    
+    if(cmd_hasFlag('A', "alt")) modifiers |= MODIFIERKEY_ALT;
+    if(cmd_hasFlag(0, "lalt")) modifiers |= MODIFIERKEY_LEFT_ALT;
+    if(cmd_hasFlag(0, "ralt")) modifiers |= MODIFIERKEY_RIGHT_ALT;
+    
+    if(cmd_hasFlag('S', "shift")) modifiers |= MODIFIERKEY_SHIFT;
+    if(cmd_hasFlag(0, "lshift")) modifiers |= MODIFIERKEY_LEFT_SHIFT;
+    if(cmd_hasFlag(0, "rshift")) modifiers |= MODIFIERKEY_RIGHT_SHIFT;
+    
+    if(cmd_hasFlag('W', "win")) modifiers |= MODIFIERKEY_GUI;
+    if(cmd_hasFlag(0, "lwin")) modifiers |= MODIFIERKEY_LEFT_GUI;
+    if(cmd_hasFlag(0, "rwin")) modifiers |= MODIFIERKEY_RIGHT_GUI;
+    
+    while(parseKey(keys, &keysCount, &parseLocation));
+    
+    if(isotope = isotope_open("/dev/ttyAMA0")) {
+        isotope_keyboard(isotope, modifiers, keys, keysCount);
+        if(release) isotope_keyboard(isotope, 0, 0, 0);
+        isotope_close(isotope);
+    } else {
+        printf("Error: Unable to connect to Isotope device, please ensure the UART is available.\n");
+        return -2;
+    }
+}
+
+typedef struct KEYBIND {
+    char shortcut[10];
+    int code;
+} KEYBIND;
+
+KEYBIND keymap[] = {
+    { "0", KEY_0 },
+    { "1", KEY_1 },
+    { "2", KEY_2 },
+    { "3", KEY_3 },
+    { "4", KEY_4 },
+    { "5", KEY_5 },
+    { "6", KEY_6 },
+    { "7", KEY_7 },
+    { "8", KEY_8 },
+    { "9", KEY_9 },
+    
+    { "A", KEY_A },
+    { "B", KEY_B },
+    { "C", KEY_C },
+    { "D", KEY_D },
+    { "E", KEY_E },
+    { "F", KEY_F },
+    { "G", KEY_G },
+    { "H", KEY_H },
+    { "I", KEY_I },
+    { "J", KEY_J },
+    { "K", KEY_K },
+    { "L", KEY_L },
+    { "M", KEY_M },
+    { "N", KEY_N },
+    { "O", KEY_O },
+    { "P", KEY_P },
+    { "Q", KEY_Q },
+    { "R", KEY_R },
+    { "S", KEY_S },
+    { "T", KEY_T },
+    { "U", KEY_U },
+    { "V", KEY_V },
+    { "W", KEY_W },
+    { "X", KEY_X },
+    { "Y", KEY_Y },
+    { "Z", KEY_Z },
+    
+    { "F1", KEY_F1 },
+    { "F2", KEY_F2 },
+    { "F3", KEY_F3 },
+    { "F4", KEY_F4 },
+    { "F5", KEY_F5 },
+    { "F6", KEY_F6 },
+    { "F7", KEY_F7 },
+    { "F8", KEY_F8 },
+    { "F9", KEY_F9 },
+    { "F10", KEY_F10 },
+    { "F11", KEY_F11 },
+    { "F12", KEY_F12 },
+    { "F13", KEY_F13 },
+    { "F14", KEY_F14 },
+    { "F15", KEY_F15 },
+    { "F16", KEY_F16 },
+    { "F17", KEY_F17 },
+    { "F18", KEY_F18 },
+    { "F19", KEY_F19 },
+    { "F20", KEY_F20 },
+    { "F21", KEY_F21 },
+    { "F22", KEY_F22 },
+    { "F23", KEY_F23 },
+    { "F24", KEY_F24 },
+    
+    { ",", KEY_COMMA },
+    { ".", KEY_PERIOD },
+    { "/", KEY_SLASH },
+    { "\\", KEY_BACKSLASH },
+    { "(", KEY_LEFT_BRACE },
+    { ")", KEY_RIGHT_BRACE },
+    { "'", KEY_QUOTE },
+    { ";", KEY_SEMICOLON },
+    { "`", KEY_TILDE },
+    
+    { "SPACE", KEY_SPACE },
+    { "BACKSPACE", KEY_BACKSPACE },
+    { "ENTER", KEY_ENTER },
+    { "ESCAPE", KEY_ESC },
+    { "ESC", KEY_ESC },
+    { "NUMLOCK", KEY_NUM_LOCK },
+    { "CAPSLOCK", KEY_CAPS_LOCK },
+    { "DEL", KEY_DELETE },
+    { "DELETE", KEY_DELETE },
+    { "END", KEY_END },
+    { "MINUS", KEY_MINUS },
+    { "EQUALS", KEY_EQUAL },
+    { "EQUAL", KEY_EQUAL },
+    { "DOWN", KEY_DOWN },
+    { "LEFT", KEY_LEFT },
+    { "RIGHT", KEY_RIGHT },
+    { "UP", KEY_UP },
+    { "HOME", KEY_HOME },
+    { "INSERT", KEY_INSERT },
+    { "MENU", KEY_MENU },
+    { "TAB", KEY_TAB },
+    { "PGUP", KEY_PAGE_UP },
+    { "PGDN", KEY_PAGE_DOWN }
+};
+
+char parseKey(char* keys, int* keysCount, int* parseLocation) {
+    char* key;
+    int i;
+    
+    key = cmd_getNextValue(parseLocation);
+    if(!key) return 0;
+    strupr(key);
+    
+    for(i = 0; i < sizeof(keymap)/sizeof(KEYBIND); i++) {
+        if(!strcmp(key, keymap[i].shortcut)) {
+            keys[*keysCount++] = keymap[i].code;
+            return 1;
+        }
+    }
+    
+    printf("WARN: Failed to find a binding for key '%s'\n", key);
+    return 1;
+}
---
+++ src/apps/isomouse.c
@@ -0,0 +1,76 @@
+/**
+ * Isotope Mouse Emulation Command Line Applet
+ * This command line executable allows emulation of
+ * basic mouse functions through an easy to understand
+ * command line syntax.
+ */
+
+/**
+ * Examples:
+ * Hold left button and move 10 left, release
+ * > isomouse -L X10
+ */
+
+#include "../libs/c/isotope.h"
+
+#include "cmdline.h"
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char** argv) {
+    int isotope;
+    char buttons = 0;
+    char x = 0, y = 0, scroll = 0;
+    int parseLocation = 1;
+    char* value;
+    int val;
+    char release = 1;
+    
+    
+    cmd_init(argc, argv);
+    
+    if(cmd_hasFlag('?', "help")) {
+        printf("Isotope Keyboard Emulator\n");
+        printf("Usage: %s -L -right -M X10 Y0 S-1\n", cmd_application());
+        return -1;
+    }
+    
+    if(cmd_hasFlag('L', "left")) buttons |= 0x1;
+    if(cmd_hasFlag('R', "right")) buttons |= 0x2;
+    if(cmd_hasFlag('M', "middle")) buttons |= 0x4;
+    if(cmd_hasFlag('H', "hold")) release = 0;
+    
+    while(value = cmd_getNextValue(&parseLocation)) {
+        sscanf(value + 1, "%d", &val);
+        
+        if(val > 127 || val < -127) {
+            if(val > 127) val = 127;
+            else val = -127;
+            printf("WARN: %c value was outside maximum bounds (-127 to 127), used %d instead.\n", value[0], val);
+        }
+        switch(value[0]) {
+            case 'X':
+            case 'x':
+                x = (char)val;
+                break;
+            case 'Y':
+            case 'y':
+                y = (char)val;
+                break;
+            case 'S':
+            case 's':
+                scroll = (char)val;
+                break;
+        }
+    }
+    
+    if(isotope = isotope_open("/dev/ttyAMA0")) {
+        isotope_mouse(isotope, buttons, x, y, scroll);
+        if(release) isotope_mouse(isotope, 0, 0, 0, 0);
+        isotope_close(isotope);
+    } else {
+        printf("Error: Unable to connect to Isotope device, please ensure the UART is available.\n");
+        return -2;
+    }
+}
